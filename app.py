05f3f5f76a077c1f10490a86d9f551da56be0372
niharika-dotcom <vemasaniniharika342@gmail.com>
2026-06-30 11:29:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import plotly.graph_objects as go
 from deep_translator import GoogleTranslator
 import hashlib
 import random
-import speech_recognition as sr
 from PIL import Image
 import os
 import base64
